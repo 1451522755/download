cbec429138bc92270f4df63ecb822fa6ea5dea22
1451522755 <1451522755@qq.com>
2022-01-28 22:08:24 +0800

    Update wireguard-go.sh

--- sh/warp/wireguard-go.sh
+++ sh/warp/wireguard-go.sh
@@ -25,7 +25,7 @@ INFO="[${Green_font_prefix}INFO${Font_color_suffix}]"
 ERROR="[${Red_font_prefix}ERROR${Font_color_suffix}]"
 
 PROJECT_NAME='wireguard-go'
-GH_API_URL='http://api.github.freenn.com/repos/P3TERX/wireguard-go-builder/releases/latest'
+GH_API_URL='http://down.freenn.com/sh/warp/warp.html'
 BIN_DIR='/usr/local/bin'
 BIN_NAME='wireguard-go'
 BIN_FILE="${BIN_DIR}/${BIN_NAME}"
